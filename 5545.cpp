354940d23ca77257d6ab4a1e260fbde8a301b89e
KDY <kdy45482016@gmail.com>
2026-04-17 09:15:11 +0900

    update

--- 5545.cpp
+++ 5545.cpp
@@ -4,17 +4,36 @@ using namespace std;
 
 typedef long long ll;
 
-int n, a, b;
+ll p, v, n;
+ll a, b, c, d;
+
+ll GCD(ll a, ll b){
+    while(b != 0){
+        auto t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+ll LCM(ll a, ll b){
+    return a * b / GCD(a, b); 
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> a >> b >> n;
+    cin >> p >> v >> n;
 
-    ll af = n / (a + 1);
-    ll bf = n / (b + 1);
+    b = n / LCM(p + 1, v + 1);
+    c = n / (v + 1) - b;
+    d = n / (p + 1) - b;
+    a = n - (b + c + d);
 
-    
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
+    cout << d << "\n";
 }
