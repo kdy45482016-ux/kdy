09cf2b64e94f25edaabe9c7598b47571e75dad8d
KDY <kdy45482016@gmail.com>
2026-07-19 12:34:28 +0900

    update

--- 5656.cpp
+++ 5656.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
 
+#define MAX 800000
+#define MIN 100000
+
 using namespace std;
 
 int n;
-vector<int> arr;
-vector<int> sum;
+vector<int> prefix;
+vector<int> cnt;
+long long ans = 0;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -13,17 +17,21 @@ int main() {
 
     cin >> n;
 
-    arr.resize(n + 1, 0);
-    sum.resize(n + 1, 0);
+    prefix.resize(n + 1, 0);
+    cnt.resize(MAX + MIN, 0);
 
     string str;
     cin >> str;
 
     for(int i = 0; i < n; i++) {
         int num = str[i] - '0';
-        arr[i + 1] = num;
-        sum[i + 1] = sum[i] + num;
+        num -= 1;
+        prefix[i + 1] = prefix[i] + num;
     }
 
-    // 
+    for(int i = 0; i <= n; i++) {
+        ans += (long long)cnt[MIN + prefix[i]]++;
+    }
+
+    cout << ans << "\n";
 }
